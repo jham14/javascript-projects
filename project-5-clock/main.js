86127c9fc1b61fb5830646e1e7a6e2d191756bb0
Josh Cunningham <josh.cunningham30@gmail.com>
2021-10-03 22:09:14 +0100

    pushed project 5

---
+++ project-5-clock/main.js
@@ -0,0 +1,28 @@
+// Build a clock using JavaScript.
+//
+// -Display the hours, minutes, and seconds.
+// -The clock should run by itself. (Hint: look at JavaScript setInterval function)
+
+setInterval(setClock, 1000) //call setClock every 1000ms
+
+const hourHand = document.querySelector('[data-hour-hand]');
+const minuteHand = document.querySelector('[data-minute-hand]');
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock() {
+  const currentDate = new Date()
+  const secondsRatio = currentDate.getSeconds() / 60
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+  setRotation(secondHand, secondsRatio)
+  setRotation(minuteHand, minutesRatio)
+  setRotation(hourHand, hoursRatio)
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
+
+console.log(new Date().getSeconds() / 60 * 360);
