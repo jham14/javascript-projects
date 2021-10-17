608ea8256ad5a5846452798b330e904d44cab0d2
Josh Cunningham <josh.cunningham30@gmail.com>
2021-10-17 22:51:20 +0100

    added todo app with local storage feature

---
+++ project-8-todo-app-w.bootstrap/main.js
@@ -0,0 +1,164 @@
+//-A user must be able to add, edit, and delete a todo item.
+
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
+const ul = document.querySelector('ul');
+
+let todoArray = [];
+
+
+//generate new HTML template in <ul>
+function generateTemplate(todo) {
+
+  const html = `
+  <li class="list-group-item d-flex" id="${todo.id}">
+  <form>
+  <input type="text" class="editField inputs" value="${todo.name}" readonly disabled="disabled">
+  <i class="fas fa-edit edit ml-auto align-self-center"></i>
+  </form>
+  <i class="far fa-trash-alt delete align-self-center"></i>
+  </li>
+  `;
+
+  list.innerHTML += html;
+
+};
+
+//listen for new todo
+addForm.addEventListener('submit', e => {
+
+  //prevent refresh
+  e.preventDefault();
+
+  //.trim() - remove white spaces entered
+  const todo = addForm.newtodo.value.trim();
+
+    //if empty string - don't fire Teplate function
+    if (todo === ''){
+      return
+    };
+
+    addTodo(todo);
+
+    //reset input field after submit
+    addForm.reset();
+
+  });
+
+//add to todoArray, update local storage and create template
+function addTodo(todo){
+  if(todo != ''){
+    const todoItem = {
+      id: Date.now(),
+      name: todo,
+    };
+
+    todoArray.push(todoItem);
+    addToLocalStorage(todoArray);
+    generateTemplate(todoItem);
+  };
+
+};
+
+//render on load
+function renderTodos() {
+
+  const list = localStorage.getItem('todos');
+  todoArray = JSON.parse(list);
+
+  todoArray.forEach((item) => {
+  generateTemplate(item)
+  });
+};
+
+//delete todo
+function deleteTodo(id){
+
+  todoArray = todoArray.filter(item => {
+
+    return item.id != id
+
+  });
+
+  addToLocalStorage(todoArray);
+
+};
+
+
+function addToLocalStorage(todos){
+  localStorage.setItem('todos', JSON.stringify(todos));
+};
+
+//render todos on page load
+if(localStorage.getItem('todos')){
+  renderTodos()
+}
+
+
+// delete todo via trash can icon - remove() method on parent element
+list.addEventListener('click', e => {
+
+  if(e.target.classList.contains('delete')){
+
+    deleteTodo(e.target.parentElement.id);
+    e.target.parentElement.remove();
+
+  }
+
+});
+
+//enable todo edit via pencil icon
+list.addEventListener('click', e => {
+
+ if(e.target.classList.contains('edit') && e.target.previousElementSibling.hasAttribute('readonly')){
+    e.target.previousElementSibling.removeAttribute('readonly');
+    e.target.previousElementSibling.disabled = false;
+    e.target.previousElementSibling.focus();
+    e.target.previousElementSibling.setSelectionRange(0, 100);
+  } else if (e.target.classList.contains('edit')) {
+    e.target.previousElementSibling.setAttribute('readonly', 'true');
+    e.target.previousElementSibling.disabled = true;
+  };
+
+});
+
+
+//edit todo & update local storage on submit
+ul.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const newNameVal = e.target.firstChild.nextSibling.value;
+  const parentId = e.target.parentElement.id;
+
+  const eventIndex = todoArray.findIndex(element => {
+    return element.id == parentId});
+
+  todoArray[eventIndex].name = newNameVal;
+  addToLocalStorage(todoArray);
+
+  e.target.firstChild.nextSibling.blur();
+  e.target.firstChild.nextSibling.disabled = true;
+
+});
+
+//search function using filter()
+function filterTodos(term){
+  Array.from(list.children)
+  .filter((todo) => !todo.innerHTML.toLowerCase().includes(term))
+  .forEach((todo) => {
+      todo.classList.add('filtered');
+    });
+
+  Array.from(list.children)
+  .filter((todo) => todo.innerHTML.toLowerCase().includes(term))
+  .forEach((todo) => {
+      todo.classList.remove('filtered');
+    })
+  };
+
+//keyup event when a key is released - fire filterTodos()
+search.addEventListener('keyup', () => {
+  const term = search.value.trim().toLowerCase();
+  filterTodos(term);
+});
